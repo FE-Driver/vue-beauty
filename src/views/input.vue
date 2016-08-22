254fec921540ace0951fd468f2e2f6f005fa6952
yaolimei <yaolimei@huoyunren.com>
2016-08-22 11:46:31 +0800

    input组件支持传入自定义前后缀

--- src/views/input.vue
+++ src/views/input.vue
@@ -31,6 +31,38 @@
           <v-input placeholder="基本使用"></v-input>
         </code-box>
 
+      </div>
+
+      <div class="ant-col-lg-12 code-boxes-col-2-1">
+        <code-box
+        title="前后缀修饰添加"
+          describe="带有前缀后缀修饰"
+          code='
+          <v-input placeholder="基本使用">
+            <span slot="before">http://</span>
+            <span slot="after">.com</span>
+          </v-input>
+          <v-input placeholder="基本使用">
+            <v-Select placeholder="请选择" style="width: 80px;" slot="before" :options="options" :value.sync="value"></v-Select>
+            <v-Select style="width: 80px;" slot="after" :options="options2" :value.sync="value"></v-Select>
+          </v-input>'>
+          <v-input placeholder="基本使用">
+            <span slot="before">http://</span>
+            <span slot="after">.com</span>
+          </v-input>
+          <br/>
+          <v-input placeholder="基本使用">
+            <v-Select placeholder="请选择" style="width: 80px;" slot="before" :options="options" :value.sync="value"></v-Select>
+            <v-Select style="width: 80px;" slot="after" :options="options2" :value.sync="value"></v-Select>
+          </v-input>
+
+        </code-box>
+      </div>
+
+    </div>
+
+    <div class="ant-row" style="margin-left: -8px; margin-right: -8px;">
+      <div class="ant-col-lg-12 code-boxes-col-2-1">
         <code-box
           title="文本域"
           describe="用于多行输入，指定 type 为一个特殊的 textarea。"
@@ -52,6 +84,7 @@
         >
           <v-input size="large" placeholder="大尺寸" style="width:200px;display:inline-block" disabled></v-input>
           <v-input placeholder="基本尺寸" style="width:200px;display:inline-block"></v-input>
+          <br/><br/>
           <v-input size="small" placeholder="小尺寸" style="width:200px;display:inline-block"></v-input>
         </code-box>
 
@@ -73,10 +106,16 @@ import vInput from '../../components/input'
 import vButton from '../../components/button'
 import codeBox from '../components/codeBox'
 import apiTable from '../components/apiTable'
+import vAlert from '../../components/alert'
+import vSelect from '../../components/select'
 
 export default {
   data: function () {
     return {
+      url:'<span type="success" message="成功提示的文案"></span>',
+      options: [{value: '1', text: 'lady'}, {value: '2', text: '小强', disabled: true}, {value: '3', text: '小明'}],
+      options2: [{value: '1', text: 'lady'}, {value: '2', text: '小强'}, {value: '3', text: '小明'}],
+      value: '3',
       apis: [{
           parameter: 'type',
           explain: '【必须】声明 input 类型，同原生 input 标签的 type 属性。另外提供 type="textarea"。',
@@ -97,6 +136,16 @@ export default {
           explain: '是否禁用状态，默认为 false',
           type: 'Bool',
           default: 'false'
+        },{
+          parameter:'slot::before',
+          explain:'input前面加前缀修饰',
+          type:'slot node',
+          default:'无'
+        },{
+          parameter:'slot::after',
+          explain:'input后面加后缀修饰',
+          type:'slot node',
+          default:'无'
         }
       ],
       disabled: true
@@ -106,7 +155,9 @@ export default {
     vInput,
     vButton,
     codeBox,
-    apiTable
+    apiTable,
+    vAlert,
+    vSelect
   }
 }
 </script>
