b3488585c39b5cd44592bd2065604f5c3e1c66a0
yuri <yuli_cd@huoyunren.com>
2016-09-28 16:42:20 +0800

    card的extra改为slot

--- components/card/card.vue
+++ components/card/card.vue
@@ -2,7 +2,11 @@
 
   <div class="ant-card" :class="{'ant-card-bordered': bordered}">
     <div class="ant-card-head" v-if="title"><h3 class="ant-card-head-title">{{title}}</h3></div>
-    <div class="ant-card-extra" v-if="extra">{{{extra}}}</div>
+    <template v-if="extra">
+      <div class="ant-card-extra">
+        <slot name="extra"></slot>
+      </div>
+    </template>
     <div class="ant-card-body" :style="bodyStyle"><slot></slot></div>
   </div>
 
@@ -11,14 +15,22 @@
 <script>
 export default {
   name: 'v-card',
+  data:()=>({
+      prefix: 'ant-card',
+      extra: false,
+  }),
   props: {
     title: String,
     bordered: {
       type: Boolean,
       default: true
     },
-    extra: String,
     bodyStyle: Object,
-  }
+  },
+  ready(){
+    if(this._slotContents && this._slotContents.extra){
+      this.extra = true;
+    }
+  },
 }
 </script>
