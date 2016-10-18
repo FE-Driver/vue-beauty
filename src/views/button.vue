51ef007f82bed1dcbc4b4cd0ca949fa62f8ba719
yuri <yuli_cd@huoyunren.com>
2016-10-18 11:46:43 +0800

    修复layout报错

--- src/views/button.vue
+++ src/views/button.vue
@@ -58,11 +58,11 @@
           describe="当需要在 Button 内嵌入 Icon 时，可以设置 icon 属性，或者直接在 Button 内使用 Icon 组件。如果想控制 Icon 具体的位置，只能直接使用 Icon 组件，而非 icon 属性。"
         >
           <v-button type='primary' shape="circle" icon="search"></v-button>
-          <v-Button type="primary" icon="search"><span>搜索</span></v-Button>
+          <v-button type="primary" icon="search"><span>搜索</span></v-button>
           <br />
           <br />
-          <v-Button type="ghost" shape="circle-outline" icon="search" /></v-button>
-          <v-Button type="ghost" icon="search"><span>搜索</span></v-Button>
+          <v-button type="ghost" shape="circle-outline" icon="search"></v-button>
+          <v-button type="ghost" icon="search"><span>搜索</span></v-button>
         </code-box>
 
         <code-box
