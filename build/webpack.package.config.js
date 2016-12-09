3e01210724dc9d803fd0b3fc6beaeca8f7a6e57b
yuri <yuri@yurideMacBook-Pro.local>
2016-12-09 18:12:32 +0800

    npm打包配置完成

---
+++ build/webpack.package.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'vue-beauty': './vb/index.js',
+        style: './vb/style/index.less'
+    },
+    output: {
+        path: path.resolve(__dirname, '../package'),
+        publicPath: '/package/',
+        library: 'vue-beauty',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    externals: {
+        vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        }
+    },
+    resolve: {
+        extensions: ['', '.js', '.vue']
+    },
+    module: {
+        loaders: [{
+            test: /\.vue$/,
+            loader: 'vue'
+        }, {
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style!css!autoprefixer'
+        }, {
+            test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+            loader: 'url?limit=8192'
+        }, {
+            test: /\.(html|tpl)$/,
+            loader: 'vue-html'
+        }]
+    }
+}
