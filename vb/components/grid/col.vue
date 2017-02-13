121d0860dc0ac5e2d2333077bb637be478ad2c70
wanglin <wanglin_cd@huoyunren.com>
2017-02-13 15:18:12 +0800

    Grid增加响应式布局接口

--- vb/components/grid/col.vue
+++ vb/components/grid/col.vue
@@ -1,28 +1,57 @@
 <template>
-  <div :class="classes">
-    <slot></slot>
-  </div>
+    <div :class="classes">
+        <slot></slot>
+    </div>
 </template>
-<script>
-  export default {
-    name:'v-col',
-    props: {
-      span: [String, Number],
-      offset: [String, Number],
-      push:[String, Number],
-      pull:[String, Number],
-      order:[String, Number]
-    },
-    computed: {
-      classes () {
-        return [
-          this.span ? `ant-col-${this.span}` : '',
-          this.offset ? `ant-col-offset-${this.offset}` : '',
-          this.push ? `ant-col-push-${this.push}` : '',
-          this.pull ? `ant-col-pull-${this.pull}` : '',
-          this.order ? `ant-col-order-${this.order}` : '',
-        ]
-      }
+<script lang="babel">
+    export default {
+        name: 'v-col',
+        props: {
+            span: [String, Number],
+            offset: [String, Number],
+            push: [String, Number],
+            pull: [String, Number],
+            order: [String, Number],
+            xs: [Number, Object],
+            sm: [Number, Object],
+            md: [Number, Object],
+            lg: [Number, Object],
+        },
+        data() {
+            return {
+                prefixCls: 'ant-col',
+            }
+        },
+        computed: {
+            classes () {
+                const props = this;
+                const prefixCls = this.prefixCls;
+                let sizeClassObj = {};
+                ['xs', 'sm', 'md', 'lg'].forEach(size => {
+                    let sizeProps = {};
+                    if (typeof props[size] === 'number') {
+                        sizeProps.span = props[size];
+                    } else if (typeof props[size] === 'object') {
+                        sizeProps = props[size] || {};
+                    }
+
+                    sizeClassObj = Object.assign({}, sizeClassObj, {
+                        [`${prefixCls}-${size}-${sizeProps.span}`]: sizeProps.span !== undefined,
+                        [`${prefixCls}-${size}-order-${sizeProps.order}`]: sizeProps.order || sizeProps.order === 0,
+                        [`${prefixCls}-${size}-offset-${sizeProps.offset}`]: sizeProps.offset || sizeProps.offset === 0,
+                        [`${prefixCls}-${size}-push-${sizeProps.push}`]: sizeProps.push || sizeProps.push === 0,
+                        [`${prefixCls}-${size}-pull-${sizeProps.pull}`]: sizeProps.pull || sizeProps.pull === 0,
+                    });
+                });
+                return [
+                    this.span ? `${prefixCls}-${this.span}` : '',
+                    this.offset ? `${prefixCls}-offset-${this.offset}` : '',
+                    this.push ? `${prefixCls}-push-${this.push}` : '',
+                    this.pull ? `${prefixCls}-pull-${this.pull}` : '',
+                    this.order ? `${prefixCls}-order-${this.order}` : '',
+                    sizeClassObj
+                ]
+            }
+        }
     }
-  }
 </script>
--- vb/components/grid/row.vue
+++ vb/components/grid/row.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="ant-row" :class="classes">
-    <slot></slot>
-  </div>
+    <div class="ant-row" :class="classes">
+        <slot></slot>
+    </div>
 </template>
-<script>
-  import { defaultProps } from '../../utils'
-  export default {
-    name:'v-row',
-    props: defaultProps({
-      gutter: Number,
-      pack:String,
-      display:String,
-      align:String
-    }),
-    ready(){
-      if(this.gutter){
-        let half = this.gutter/2;
-        this.$el.style.marginLeft = -half + 'px';
-        this.$el.style.marginRight= -half + 'px';
+<script lang="babel">
+    import {defaultProps} from '../../utils'
+    export default {
+        name: 'v-row',
+        props: defaultProps({
+            gutter: Number,
+            pack: String,
+            display: String,
+            align: String
+        }),
+        ready(){
+            if (this.gutter) {
+                let half = this.gutter / 2;
+                this.$el.style.marginLeft = -half + 'px';
+                this.$el.style.marginRight = -half + 'px';
 
-        for(let $child of this.$children){
-          $child.$el.style.paddingLeft = half + 'px';
-          $child.$el.style.paddingRight = half + 'px';
+                for (let $child of this.$children) {
+                    $child.$el.style.paddingLeft = half + 'px';
+                    $child.$el.style.paddingRight = half + 'px';
+                }
+            }
+        },
+        computed: {
+            classes () {
+                return [
+                    this.display ? `ant-row-${this.display}` : '',
+                    this.pack ? `ant-row-flex-${this.pack}` : '',
+                    this.align ? `ant-row-flex-${this.align}` : '',
+                ]
+            }
         }
-      }
-    },
-    computed: {
-      classes () {
-        return [
-          this.display ? `ant-row-${this.display}` : '',
-          this.pack ? `ant-row-flex-${this.pack}` : '',
-          this.align ? `ant-row-flex-${this.align}` : '',
-        ]
-      }
-    }
 
-  }
+    }
 </script>
 
