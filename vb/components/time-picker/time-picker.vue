0208d321750401b3cdc4c104314d57068d5c11dd
yuri <yuri@yurideMacBook-Pro.local>
2017-08-03 15:28:30 +0800

    timepicker:符合eslint规范

--- vb/components/time-picker/time-picker.vue
+++ vb/components/time-picker/time-picker.vue
@@ -29,73 +29,73 @@
                 dropDown: false,
                 container: null,
                 defaultValue: this.value,
-                selected: false
-            }
+                selected: false,
+            };
         },
         props: {
             placeholder: {
                 type: String,
-                default: ()=>t('timePicker.placeholder')
+                default: () => t('timePicker.placeholder'),
             },
             popupContainer: {
                 type: Function,
-                default: ()=> document.body
+                default: () => document.body,
             },
             position: {
                 type: String,
-                default: 'absolute'
+                default: 'absolute',
             },
             size: String,
             format: String,
             disabled: Boolean,
             value: {
                 type: String,
-                default: ''
+                default: '',
             },
             hideDisabledOptions: {
                 type: Boolean,
-                default: false
+                default: false,
             },
             disabledHours: Function,
             disabledMinutes: Function,
-            disabledSeconds: Function
+            disabledSeconds: Function,
         },
         computed: {
-            wrapClasses (){
+            wrapClasses() {
                 return [
                     this.prefix,
-                    `${this.prefix}-${this.size}`
-                ]
-            }
+                    `${this.prefix}-${this.size}`,
+                ];
+            },
         },
-        mounted (){
+        mounted() {
             this.container = this.popupContainer();
             this.$refs.timePickerOption.$el.style.position = this.position;
             this.container.appendChild(this.$refs.timePickerOption.$el);
 
-            this.$nextTick(()=>{
+            this.$nextTick(() => {
                 this.setPosition();
-            })
+            });
             let timer = null;
-            window.addEventListener('resize', ()=> {
+            window.addEventListener('resize', () => {
                 clearTimeout(timer);
-                timer = setTimeout(()=> {
+                timer = setTimeout(() => {
                     if (!this.disabled) {
                         this.setPosition();
                     }
-                }, 200)
-            })
+                }, 200);
+            });
 
-            if(this.$refs.addon){
+            if (this.$refs.addon) {
                 this.$refs.timePickerOption.$el.children[0].appendChild(this.$refs.addon);
             }
         },
-        beforeDestroy (){
+        beforeDestroy() {
             this.container.removeChild(this.$refs.timePickerOption.$el);
         },
         watch: {
-            defaultValue(val){
-                this.$emit('input',val)
+            defaultValue(val) {
+                this.$emit('input', val);
                 this.dispatch('FormItem', 'form.change', [val]);
             },
             value() {
@@ -103,27 +103,25 @@
             },
         },
         methods: {
-            optionClose (){
+            optionClose() {
                 this.selected = false;
             },
-            toggleDropdown (){
+            toggleDropdown() {
                 this.selected = !this.selected;
-                if(this.selected){
-                    this.$nextTick(()=>{
+                if (this.selected) {
+                    this.$nextTick(() => {
                         this.setPosition();
-                    })
+                    });
                 }
             },
-            setPosition (){
-                if(!this.$el){
-                    return
-                }
-                let p = getOffset(this.$refs.timePicker, this.container);
+            setPosition() {
+                if (!this.$el) return;
+                const p = getOffset(this.$refs.timePicker, this.container);
 
                 this.style = {
-                    top: p.top + 'px',
-                    left: p.left + 'px'
-                }
+                    top: `${p.top}px`,
+                    left: `${p.left}px`,
+                };
             },
             closeDropdown() {
                 this.selected = false;
@@ -132,5 +130,5 @@
         components: {
             timePickerOption,
         },
-    }
+    };
 </script>
