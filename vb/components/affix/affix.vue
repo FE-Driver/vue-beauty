01f8b6754ea8d15f3ef9de9c1d46cd4b206985e0
yuri <yuri@yurideMacBook-Pro.local>
2017-09-19 15:52:50 +0800

    affix:符合eslint规范

--- vb/components/affix/affix.vue
+++ vb/components/affix/affix.vue
@@ -6,92 +6,88 @@
     </div>
 </template>
 
-<script lang="babel">
-    import {getOffset,getScroll} from "../../utils/fn"
+<script>
+    import { getOffset } from '../../utils/fn';
 
     export default {
         name: 'Affix',
-        props:{
+        props: {
             offsetTop: Number,
-            offsetBottom: Number
+            offsetBottom: Number,
         },
-        data(){
+        data() {
             return {
                 _offsetTop: Number,
                 _offsetBottom: Number,
                 prefix: 'ant-affix',
                 isTop: true,
-                isAffix: false
-            }
-
+                isAffix: false,
+            };
         },
-        mounted () {
-            this.$nextTick(()=>{
-                this._offsetTop = this.offsetTop
-                this._offsetBottom = this.offsetBottom
-                if(this._offsetTop === undefined){
-                    if( this._offsetBottom === undefined){
+        mounted() {
+            this.$nextTick(() => {
+                this._offsetTop = this.offsetTop;
+                this._offsetBottom = this.offsetBottom;
+                if (this._offsetTop === undefined) {
+                    if (this._offsetBottom === undefined) {
                         this._offsetTop = 0;
-                    }else{
+                    } else {
                         this.isTop = false;
                     }
                 }
                 document.addEventListener('scroll', this.handleScroll);
                 document.addEventListener('resize', this.handleScroll);
-            })
+            });
         },
         beforeDestroy() {
             document.removeEventListener('scroll', this.handleScroll);
             document.removeEventListener('resize', this.handleScroll);
         },
         computed: {
-            placeholderStyle(){
+            placeholderStyle() {
                 return this.isAffix ? {
-                    width: this.$el.offsetWidth + 'px',
-                    height: this.$el.offsetHeight + 'px'
-                } : null
+                    width: `${this.$el.offsetWidth}px`,
+                    height: `${this.$el.offsetHeight}px`,
+                } : null;
             },
-            affixStyle(){
+            affixStyle() {
                 let style = null;
-                if(this.isAffix ){
+                if (this.isAffix) {
                     style = {
-                        left: getOffset(this.$el).left + 'px',
-                        width: this.$el.offsetWidth + 'px',
-                    }
-                    if(this.isTop){
-                        style.top = this._offsetTop + 'px';
-                    }else{
-                        style.bottom = this._offsetBottom + 'px';
+                        left: `${getOffset(this.$el).left}px`,
+                        width: `${this.$el.offsetWidth}px`,
+                    };
+                    if (this.isTop) {
+                        style.top = `${this._offsetTop}px`;
+                    } else {
+                        style.bottom = `${this._offsetBottom}px`;
                     }
                 }
                 return style;
-            }
+            },
         },
         watch: {
-            isAffix(val){
+            isAffix(val) {
                 this.$emit('change', val);
-            }
+            },
         },
         methods: {
-            handleScroll(){
+            handleScroll() {
                 let isAffix = false;
-                const scrollTop = getScroll(true);
-                const fixedNode = this.$el.children[0];
                 const elemOffset = this.$el.getBoundingClientRect();
 
-                if(this.isTop){
-                    if(elemOffset.top < this._offsetTop){
+                if (this.isTop) {
+                    if (elemOffset.top < this._offsetTop) {
                         isAffix = true;
                     }
-                }else{
+                } else {
                     const clientH = document.documentElement.clientHeight;
-                    if(clientH - elemOffset.bottom < this._offsetBottom){
+                    if (clientH - elemOffset.bottom < this._offsetBottom) {
                         isAffix = true;
                     }
-
                 }
-                this.isAffix  = isAffix;
-            }
-        }
-    }
+                this.isAffix = isAffix;
+            },
+        },
+    };
 </script>
--- vb/components/affix/index.js
+++ vb/components/affix/index.js
@@ -1,4 +1,4 @@
-import "./style/index.less"
+import './style/index.less';
 import affix from './affix';
 
 export default affix;
